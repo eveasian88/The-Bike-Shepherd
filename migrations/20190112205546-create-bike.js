cf9f1d2c6d9442b750e37072e6af2f7c07b62e64
Scott Ratigan <scottratigan@calvisitor-10-105-177-102.calvisitor.1918.berkeley.edu>
2019-01-12 13:28:45 -0800

    updated yet again to fix travis build

--- migrations/20190112205546-create-bike.js
+++ migrations/20190112205546-create-bike.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('bikes', {
+    return queryInterface.createTable("bikes", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('bikes');
+    return queryInterface.dropTable("bikes");
   }
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,9 +16,9 @@ module.exports = function(app) {
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
