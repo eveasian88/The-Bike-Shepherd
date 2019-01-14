800ce928329e4c233d95d42f4443eeaeac19a9f6
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-01-14 11:16:20 -0800

    main.handlebars page information

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var morgan = require("morgan");
 
 var db = require("./models");
 
@@ -11,6 +12,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(morgan("dev"));
 
 // Handlebars
 app.engine(
