d81f7eff2067426769b3f22de97721fad0907667
michaelwhittemore <michaelwhittemore@gmail.com>
2019-01-15 18:52:46 -0800

    added a model for bikes and a connection file it will require creating a new schema called bikesdb

--- public/js/bikeRegistration.js
+++ public/js/bikeRegistration.js
@@ -2,35 +2,6 @@
 //bike registration data
 
 $("#signupbtn").on("click",function(event){
-<<<<<<< HEAD
-  event.preventDefault();
-  let userID=""; //TODO grab from local storage
-  let nickname=$("#nickname").val().trim();
-  let color=$("#color").val().trim();
-  let brand=$("#brand").val().trim();
-  let serialNumber=$("#serialNumber").val().trim();
-  let model=$("#model").val().trim();
-    console.log(nickname)
-  //the html forms should have the "required" tag to ensure that
-  //they are filled out
-
-  //create a object and then post it to the server via ajex where
-  //it will be sequlized into the database
-  let newBike={
-    userID:userID,
-    nickname:nickname,
-    color:color,
-    brand:brand,
-    serialNumber:serialNumber,
-    model:model,
-  }
-   $.post("api/register",newBike,function(data){
-    //console log to verify that we posted to server sucessfully
-    console.log("Sent bike data to server \n")
-    console.log(newBike)
-   })
-})
-=======
     event.preventDefault();
     let userID=""; //TODO grab from local storage
     let nickname=$("#nickname").val().trim();
@@ -38,7 +9,6 @@ $("#signupbtn").on("click",function(event){
     let brand=$("#brand").val().trim();
     let serialNumber=$("#serialNumber").val().trim();
     let model=$("#model").val().trim();
-      console.log(nickname)
     //the html forms should have the "required" tag to ensure that
     //they are filled out
   
@@ -58,4 +28,3 @@ $("#signupbtn").on("click",function(event){
       console.log(newBike)
      })
   })
->>>>>>> 89d31a53a3563c021794a2fb3f61e76ca7e537ac
