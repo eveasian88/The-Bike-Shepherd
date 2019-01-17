4b09579d25f734c231275353f272ae800cd7c6d2
michaelwhittemore <michaelwhittemore@gmail.com>
2019-01-16 21:42:14 -0800

    connection should work

--- models/bike.js
+++ models/bike.js
@@ -1,20 +1,11 @@
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
-
-// Creates a "Bike" model that matches up with DB
-var Bike = sequelize.define("bike", {
-  userID: Sequelize.STRING,
-  nickname: Sequelize.STRING,
-  color: Sequelize.STRING,
-  brand: Sequelize.STRING,
-  serialNumber: Sequelize.STRING,
-  model: Sequelize.STRING
-});
-
-// Syncs with DB
-Bike.sync();
-
-// Makes the Bike Model available for other files (will also create a table)
-module.exports = Bike;
+module.exports = function(sequelize, DataTypes) {
+  var Bike = sequelize.define("bike", {
+    userID: DataTypes.STRING,
+    nickname: DataTypes.STRING,
+    color: DataTypes.STRING,
+    brand: DataTypes.STRING,
+    serialNumber: DataTypes.STRING,
+    model: DataTypes.STRING
+  });
+  return Bike;
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,18 +2,18 @@ var db = require("../models");
 
 module.exports = app => {
   // Get all examples
-  app.get("/api/examples", (req, res) => {
-    db.Example.findAll({}).then(dbExamples => {
-      res.json(dbExamples);
-    });
-  });
+  // app.get("/api/examples", (req, res) => {
+  //   db.Example.findAll({}).then(dbExamples => {
+  //     res.json(dbExamples);
+  //   });
+  // });
 
-  // Create a new example
-  app.post("/api/examples", (req, res) => {
-    db.Example.create(req.body).then(dbExample => {
-      res.json(dbExample);
-    });
-  });
+  // // Create a new example
+  // app.post("/api/examples", (req, res) => {
+  //   db.Example.create(req.body).then(dbExample => {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   // Delete an example by id
   // app.delete("/api/examples/:id", (req, res) => {
